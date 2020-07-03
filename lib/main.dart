10920d65952b389c7295f3d79a308d6c1258917b
dawidschatton <g5ytgrduchd33yb2xd6qjy2g6egpeabthf7sblqjtxn553zhmb3a>
2020-07-03 21:36:41 +0200

    Added downloading repo and unzip + run script

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'dart:io' as io;
+import 'dart:typed_data';
+import 'package:archive/archive_io.dart';
 import 'package:file_chooser/file_chooser.dart' as fc;
 import 'package:flutter/material.dart';
 import 'package:process_run/shell.dart' as shell;
+import 'package:process_run/src/user_config.dart';
+import 'package:process_run/process_run.dart' as process_run;
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -16,31 +22,89 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainPage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class MainPage extends StatefulWidget {
+  MainPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainPageState createState() => _MainPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class VALUES {
+  static String gitRepositoryUrl =
+      'https://api.github.com/repos/pawelpluta/kup-script/zipball/';
+  static String commit = 'master';
+  static String zipFileName = 'script.zip';
+  static String kupScriptDirectory = 'kup-script';
+}
+
+class _MainPageState extends State<MainPage> {
+  bool isDownloading = false;
   String repositoryPath = '';
-  String kupScriptPath = '';
+  String currentDir = '';
+
   @override
   void initState() {
     super.initState();
+    path_provider.getApplicationSupportDirectory().then((value) {
+      if (!io.File('${value.path}/${VALUES.zipFileName}').existsSync()) {
+        _getKupScript(value.path);
+      }
+    });
+  }
+
+  void _isDownloading(state) {
+    setState(() {
+      isDownloading = state;
+    });
+  }
+
+  void _getKupScript(path) async {
+    _isDownloading(true);
+    http.Response res = await http.get(
+      VALUES.gitRepositoryUrl + VALUES.commit,
+      headers: {'Accept': 'application/vnd.github.v3+json'},
+    );
+    io.File file = io.File('$path/script.zip');
+    await file.writeAsBytes(res.bodyBytes);
+    _unzipAndSave(path);
+    _isDownloading(false);
+  }
+
+  void _unzipAndSave(path) {
+    Uint8List zip = io.File('$path/${VALUES.zipFileName}').readAsBytesSync();
+    Archive files = ZipDecoder().decodeBytes(zip);
+    var replaceStr = files[0].name;
+    io.Directory('$path/${VALUES.kupScriptDirectory}').createSync();
+
+    files.forEach((element) {
+      var filename = element.name.replaceFirst(RegExp(replaceStr), '');
+      if (element.isFile) {
+        var data = element.content as List<int>;
+        var file = io.File('$path/${VALUES.kupScriptDirectory}/${filename}');
+        file.createSync(recursive: true);
+        file.writeAsBytesSync(data);
+      } else {
+        io.Directory('$path/${VALUES.kupScriptDirectory}/${filename}')
+            .createSync();
+      }
+    });
+  }
+
+  Future<String> _readFile(String fileName) async {
+    var path = await path_provider.getApplicationSupportDirectory();
+    return io.File('${path.path}/${VALUES.kupScriptDirectory}/$fileName')
+        .readAsStringSync();
   }
 
   @override
   Widget build(BuildContext context) {
-    print(io.Directory.current);
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(24.0),
@@ -48,6 +112,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
+            isDownloading ? Text('Downloading scripts!') : Text(''),
             Text(
               'Repository path:',
               style: TextStyle(color: Colors.grey),
@@ -56,100 +121,46 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(
               height: 6.0,
             ),
-            Text(
-              'Kup-Script path:',
-              style: TextStyle(color: Colors.grey),
-            ),
-            _buildSelectKupScriptButton(),
             SizedBox(
               height: 6.0,
             ),
             OutlineButton.icon(
               textColor: Colors.green[700],
               padding: EdgeInsets.symmetric(horizontal: 26.0, vertical: 16.0),
-              onPressed: repositoryPath != '' && kupScriptPath != ''
+              onPressed: repositoryPath == ''
                   ? () async {
-                      String p = '$kupScriptPath/main.sh';
-                      List arg = [repositoryPath, "2020-01-01", "2020-02-31"];
-
                       var s = shell.Shell();
-                      var r = await s.run(p);
-                      print(r.toList());
+                      //print(await _readFile('cleanup.sh'));
+                      var userPaths = getUserPaths(<String, String>{});
+                      // for (var path in userPaths) {
+                      //   print(path);
+                      // }
+
+                      await s.run('''
+                        # Display some text
+                        echo Hello
+                        # Display dart version
+                        dart --version
+                        # Display pub version
+                        pub --version
+                      ''');
+
+                      s = s.pushd('example');
+
+                      await s.run('''
+# Listing directory in the example folder
+dir
+  ''');
+                      s = s.popd();
                     }
                   : null,
               icon: Icon(Icons.autorenew),
               label: Text('Generate list'),
             ),
-            Container(
-              child: Table(
-                children: [
-                  TableRow(
-                    children: [
-                      Text(
-                        'R',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'fewfe',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                  TableRow(
-                    decoration: BoxDecoration(),
-                    children: [
-                      Text('fwfe'),
-                      Text('fewfe'),
-                    ],
-                  )
-                ],
-              ),
-            )
+            Text(currentDir),
           ],
         ),
       ),
-    );
-  }
-
-  Row _buildSelectKupScriptButton() {
-    return Row(
-      children: [
-        OutlineButton.icon(
-          textColor: Colors.blue[700],
-          padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
-          onPressed: () async {
-            var paths = await fc.showOpenPanel(
-              canSelectDirectories: true,
-              allowsMultipleSelection: false,
-            );
-
-            if (!paths.canceled) {
-              setState(() {
-                kupScriptPath = paths.paths[0];
-              });
-            }
-          },
-          icon: Icon(Icons.folder_open),
-          label: Text(
-            'Select a folder',
-            style: TextStyle(fontSize: 10.0),
-          ),
-        ),
-        SizedBox(
-          width: 10.0,
-        ),
-        Text(
-          kupScriptPath,
-          style: TextStyle(
-            fontSize: 12.0,
-            fontWeight: FontWeight.w600,
-          ),
-        )
-      ],
     );
   }
 
@@ -188,25 +199,5 @@ class _MyHomePageState extends State<MyHomePage> {
         )
       ],
     );
-  }
-}
-
-class CustomPath {
-  String path;
-  String name;
-  bool isDirectory;
-
-  CustomPath(this.path, this.name, this.isDirectory);
-
-  static bool isPathDirectory(p) {
-    return !io.File(p).existsSync();
-  }
-
-  static String pathName(p) {
-    return p.split('/').last;
-  }
-
-  static customPath(String newPath) {
-    return CustomPath(newPath, pathName(newPath), isPathDirectory(newPath));
   }
 }
